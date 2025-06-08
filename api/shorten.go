48050ac17926bdfe71e51b61f7673e647fa857c3
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-08 12:47:58 +0530

    more cleanup

---
+++ api/shorten.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+
+)
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit_"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func (app *application) ShortenURL() error {
+	return nil
+	
+}

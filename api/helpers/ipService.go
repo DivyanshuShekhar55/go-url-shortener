e1d0481f9ed688aadf6f516628541f85b03b0d8b
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-08 17:47:29 +0530

    added helper for getting ip addr from req

---
+++ api/helpers/ipService.go
@@ -0,0 +1,20 @@
+package helper
+
+import (
+	"net/http"
+)
+
+func getIPClient(w http.ResponseWriter, r *http.Request) (ip string) {
+
+	ip = r.RemoteAddr
+
+	// if behind a proxy or load balancer
+	// check for X-Forwarded-For header
+	forwarded := r.Header.Get("X-Forwarded-For")
+	if forwarded != "" {
+		ip = forwarded
+	}
+
+	return ip
+
+}

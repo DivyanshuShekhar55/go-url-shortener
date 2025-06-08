b97a2789e1aa1199d105e79431c592c89bb9774a
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-08 12:44:00 +0530

    switched to net/http

--- api/main.go
+++ api/main.go
@@ -1,6 +1,42 @@
-package main 
+package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+)
 
-func main(){
-	
+type application struct {
+	ctx  context.Context
+	addr string
+	mux  http.Handler
+}
+
+func main() {
+
+	ctx := context.Background()
+
+	mux := http.NewServeMux()
+
+	app := application{
+		ctx:  ctx,
+		addr: ":8000",
+		mux:  mux,
+	}
+
+	srv := &http.Server{
+		Addr:         app.addr,
+		Handler:      app.mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("error : server couldn't run")
+		return
+	}
+
 }

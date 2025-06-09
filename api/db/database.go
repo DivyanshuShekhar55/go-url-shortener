abb0509e885c15f524600b8a99d2969380006392
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-09 11:05:05 +0530

    updated with separate client config

--- api/db/database.go
+++ api/db/database.go
@@ -9,10 +9,30 @@ import (
 
 var Db_ctx = context.Background()
 
-func CreateClient(dbNo int) *redis.Client {
+func CreateWriteClient(dbNo int) *redis.Client {
 	redis_db := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("DB_ADDR"),
-		Password: os.Getenv("DB_PASS"),
+		Addr:     os.Getenv("WRITE_DB_ADDR"),
+		Password: os.Getenv("WRITE_DB_PASS"),
+		DB:       dbNo,
+	})
+
+	return redis_db
+}
+
+func CreateReadClient(dbNo int) *redis.Client {
+	redis_db := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("READ_DB_ADDR"),
+		Password: os.Getenv("READ_DB_PASS"),
+		DB:       dbNo,
+	})
+
+	return redis_db
+}
+
+func CreateAnalyticsClient(dbNo int) *redis.Client {
+	redis_db := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("ANALYTICS_DB_ADDR"),
+		Password: os.Getenv("ANALYTICS_DB_PASS"),
 		DB:       dbNo,
 	})
 

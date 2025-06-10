805fc186a054ea92489eaf3645b9170f557da2cb
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-10 13:27:23 +0530

    added flush logic properly

--- api/db/flusher.go
+++ api/db/flusher.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"log"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 	//"github.com/redis/go-redis/v9"
 )
 
-func StartAnalyticsFlush(shutdown chan struct{}) {
+type AnalyticsDBImpl struct {
+	Client *redis.Client
+}
+
+func (analytics_db *AnalyticsDBImpl) StartAnalyticsFlush(shutdown chan struct{}, write_db *WriteDbImpl, read_db *ReadDB) {
 	ticker := time.NewTicker(60 * time.Second)
 
 	// start flushing via a background worker goroutine
@@ -18,7 +24,7 @@ func StartAnalyticsFlush(shutdown chan struct{}) {
 			case <-ticker.C:
 
 				// 60 sseconds up, flush one batch of data
-				if err := flushAnalytics(); err != nil {
+				if err := flushAnalytics(analytics_db, write_db, read_db); err != nil {
 					log.Printf("error flushing analytics at time %s. Error : %v", time.Now(), err)
 				}
 
@@ -27,7 +33,7 @@ func StartAnalyticsFlush(shutdown chan struct{}) {
 				// a shutdown msg received from server, close ticker
 				// also add all the remaining data to write_client
 				ticker.Stop()
-				if err := flushAnalytics(); err != nil {
+				if err := flushAnalytics(analytics_db, write_db, read_db); err != nil {
 					log.Printf("Error during final analytics flush: %v", err)
 				}
 
@@ -41,23 +47,22 @@ func StartAnalyticsFlush(shutdown chan struct{}) {
 }
 
 // private function to flush from analytics_client to write_client
-func flushAnalytics() error{
+func flushAnalytics(analytics_db *AnalyticsDBImpl, write_db *WriteDbImpl, read_db *ReadDB) error {
 	ctx := context.Background()
-	var cursor uint64 
+	var cursor uint64
 
 	// init the db write and analytics clients
-	analytics_server:= CreateAnalyticsClient(0)
-	defer analytics_server.Close()
+	// analytics_server:= CreateAnalyticsClient(0)
+	// defer analytics_server.Close()
 
-	write_server := CreateWriteClient(0)
-	defer write_server.Close()
-
+	// write_server := CreateWriteClient(0)
+	// defer write_server.Close()
 
 	for {
 		// scan the analytics for all keys matching `analytics:...` pattern
 		// getting 100 results per batch
 		// keep calling the function till returned new_cursor is 0
-		keys, new_cursor, err := analytics_server.Scan(ctx, cursor, "analytics:*", 100).Result()
+		keys, new_cursor, err := analytics_db.Client.Scan(ctx, cursor, "analytics:*", 100).Result()
 
 		if err != nil {
 			log.Fatal("error getting analytics value for batch")
@@ -65,16 +70,36 @@ func flushAnalytics() error{
 		}
 
 		// for all returned keys get the values
-		for _, key := range keys{
-			count_for_url, err := analytics_server.Get(ctx, key).Int64()
+		for _, key := range keys {
+			count_for_url, err := analytics_db.Client.Get(ctx, key).Int64()
 			if err != nil {
-				fmt.Println("Couldn't get count for url %s during flushing at time %s", key[10:], time.Now())
+				fmt.Printf("Couldn't get count for url %v during flushing at time %v", key[10:], time.Now())
+			}
+
+			// read the custom short from the read db client
+			val, err := read_db.Client.Get(ctx, "CustomShort").Result()
+			if err != nil {
+				log.Fatalf("Service unreachable")
+				return err
 			}
 
 			// put them in the write db
-			fmt.Println(count_for_url, new_cursor)
-		}
+			id, err := write_db.InsertAnalytics(Analytics_req{
+				CustomShort: val,
+				Visitors:    int(count_for_url),
+			}, Db_ctx)
 
+			if err!= nil {
+				// do something
+			}
+
+			fmt.Printf("returned id %d", id)
+		}
+		if new_cursor == 0 {
+			break
+		}
+		cursor = new_cursor
 	}
 
+	return nil
 }

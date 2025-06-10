b47a9b9f0666b9a7bbcf6424db7b2fdba6af3471
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-10 19:52:56 +0530

    redis fallback for resolver api

--- api/resolve.go
+++ api/resolve.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/DivyanshuShekhar55/go-url-shortener/db"
 	"github.com/redis/go-redis/v9"
@@ -11,31 +12,70 @@ func (app *application) ResolveURL(w http.ResponseWriter, r *http.Request) {
 
 	// get the short from the url
 	url := r.URL.Query().Get("url")
+	if url == "" {
+        http.Error(w, "missing url parameter", http.StatusBadRequest)
+        return
+    }
 
 	// query the db to find the original URL, if a match is found
 	// increment the redirect counter and redirect to the original URL
 	// else return error message
-	read_db := app.read_db
 
-	value, err := read_db.Client.Get(db.Db_ctx, url).Result()
+	value, err := app.read_db.Client.Get(db.Db_ctx, url).Result()
 	if err == redis.Nil {
-		http.Error(w, "Url not found", http.StatusNotFound)
+		// key not found in redis
+		// FALLBACK : READ FROM POSTGRES
+		value, err = app.readFrom_WriteDB(url)
+		if err != nil {
+			http.Error(w, "could not find url", http.StatusNotFound)
+			return
+		}
+
+		// Also put this KV in Redis
+		// if err occurs during this step, its fine
+		// we don't need to return error
+		/* TO-DO : SET EXPIRY BASED ON USER SETTINGS */
+		_ = app.read_db.Client.Set(app.ctx, url, value, time.Hour).Err()
 
 	} else if err != nil {
-		http.Error(w, "Cannot Connect to DB", http.StatusInternalServerError)
+		http.Error(w, "Cannot Connect to Redis DB", http.StatusInternalServerError)
 
+		// FALLBACK : READ FROM POSTGRES
+		value, err = app.readFrom_WriteDB(url)
+		if err != nil {
+			http.Error(w, "could not find url", http.StatusNotFound)
+			return
+		}
 	}
-	// increment the counter for analytics
-	analytics_db := app.analytics_db
 
+	// increment the counter for analytics
 	// even if we fail to increment counter
 	// it should't restrict user from accessing the site
-	
+
 	// generate a unique key for each url, so we can use it in analytics
 	analytics_key := "analytics:" + url
-	_ = analytics_db.Client.Incr(db.Db_ctx, analytics_key)
+	_ = app.analytics_db.Client.Incr(db.Db_ctx, analytics_key)
 
 	// redirect to original URL
 	http.Redirect(w, r, value, 301)
+
+}
+
+func (app *application) readFrom_WriteDB(url string) (customShort string, err error) {
+
+	query := `
+		SELECT CustomShort
+		FROM url
+		WHERE URL = $1
+	`
+
+	err = app.write_db.Conn.QueryRow(app.ctx, query, url).Scan(&customShort)
+
+	if err != nil {
+		// no value was found, return fatal error from here
+		return "", err
+	}
+
+	return customShort, nil
 
 }

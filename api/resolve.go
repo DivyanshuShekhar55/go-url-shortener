f041534021b1da675f5967bcbba068681bc6febb
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-10 20:13:41 +0530

    fixed resolve func with expiry return value

--- api/resolve.go
+++ api/resolve.go
@@ -8,14 +8,19 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+type redis_fallback_resp struct {
+	CustomShort string
+	Expiry      time.Duration
+}
+
 func (app *application) ResolveURL(w http.ResponseWriter, r *http.Request) {
 
 	// get the short from the url
 	url := r.URL.Query().Get("url")
 	if url == "" {
-        http.Error(w, "missing url parameter", http.StatusBadRequest)
-        return
-    }
+		http.Error(w, "missing url parameter", http.StatusBadRequest)
+		return
+	}
 
 	// query the db to find the original URL, if a match is found
 	// increment the redirect counter and redirect to the original URL
@@ -25,27 +30,28 @@ func (app *application) ResolveURL(w http.ResponseWriter, r *http.Request) {
 	if err == redis.Nil {
 		// key not found in redis
 		// FALLBACK : READ FROM POSTGRES
-		value, err = app.readFrom_WriteDB(url)
+		fallback_resp, err := app.readFrom_WriteDB(url)
 		if err != nil {
 			http.Error(w, "could not find url", http.StatusNotFound)
 			return
 		}
+		value = fallback_resp.CustomShort
 
 		// Also put this KV in Redis
 		// if err occurs during this step, its fine
 		// we don't need to return error
-		/* TO-DO : SET EXPIRY BASED ON USER SETTINGS */
-		_ = app.read_db.Client.Set(app.ctx, url, value, time.Hour).Err()
+		_ = app.read_db.Client.Set(app.ctx, url, value, fallback_resp.Expiry).Err()
 
 	} else if err != nil {
 		http.Error(w, "Cannot Connect to Redis DB", http.StatusInternalServerError)
 
 		// FALLBACK : READ FROM POSTGRES
-		value, err = app.readFrom_WriteDB(url)
+		fallback_resp, err := app.readFrom_WriteDB(url)
 		if err != nil {
 			http.Error(w, "could not find url", http.StatusNotFound)
 			return
 		}
+		value = fallback_resp.CustomShort
 	}
 
 	// increment the counter for analytics
@@ -57,25 +63,31 @@ func (app *application) ResolveURL(w http.ResponseWriter, r *http.Request) {
 	_ = app.analytics_db.Client.Incr(db.Db_ctx, analytics_key)
 
 	// redirect to original URL
-	http.Redirect(w, r, value, 301)
+	http.Redirect(w, r, value, http.StatusFound)
 
 }
 
-func (app *application) readFrom_WriteDB(url string) (customShort string, err error) {
+func (app *application) readFrom_WriteDB(url string) (resp redis_fallback_resp, err error) {
 
 	query := `
-		SELECT CustomShort
+		SELECT CustomShort, Expiry
 		FROM url
 		WHERE URL = $1
 	`
 
-	err = app.write_db.Conn.QueryRow(app.ctx, query, url).Scan(&customShort)
+	var customShort string
+	var expiry time.Duration
+
+	err = app.write_db.Conn.QueryRow(app.ctx, query, url).Scan(&customShort, &expiry)
 
 	if err != nil {
 		// no value was found, return fatal error from here
-		return "", err
+		return redis_fallback_resp{}, err
 	}
 
-	return customShort, nil
+	return redis_fallback_resp{
+		CustomShort: customShort,
+		Expiry:      expiry,
+	}, nil
 
 }
